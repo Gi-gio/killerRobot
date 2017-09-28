38e26526cf468a2ba8faed4db8e5bd69ea013fab
Adrian Bucher <adrian.bucher@stud.hslu.ch>
2017-09-28 15:48:34 +0200

    led 1 implemented

--- Sources/main.c
+++ Sources/main.c
@@ -30,6 +30,8 @@
 /* Including needed modules to compile this module/procedure */
 #include "Cpu.h"
 #include "Events.h"
+#include "LED1.h"
+#include "BitIoLdd1.h"
 /* Including shared modules, which are used for whole project */
 #include "PE_Types.h"
 #include "PE_Error.h"
@@ -58,7 +60,7 @@ int main(void)
   /*** End of RTOS startup code.  ***/
   /*** Processor Expert end of main routine. DON'T MODIFY THIS CODE!!! ***/
   for(;;){
-	  int i = 5;
+	  LED1_PutVal(0);
   }
   /*** Processor Expert end of main routine. DON'T WRITE CODE BELOW!!! ***/
 } /*** End of main routine. DO NOT MODIFY THIS TEXT!!! ***/
